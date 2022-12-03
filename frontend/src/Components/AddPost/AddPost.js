58dff190bbe1408241e225319a1f79948a153d72
TudorTodea <tudortodea1416@gmail.com>
2022-12-03 04:04:20 +0200

    minor changes in add post component

--- frontend/src/Components/AddPost/AddPost.js
+++ frontend/src/Components/AddPost/AddPost.js
@@ -3,53 +3,59 @@ import { useNavigate, useParams } from 'react-router-dom';
 import './AddPost.css'
 import axios from 'axios';
 import { toastOptions } from '../../Utils/Toastify';
-import {toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 const AddPost = () => {
-  const[postData,setPostData]=useState({});
-  const {communityName}=useParams();
-  const navigate=useNavigate();
-  const variables={Name:communityName,
-  writer:localStorage.getItem('userid'),
-  title:postData.title,
-  content:postData.content
-}
-  const addPostHandler=async(e)=>{
-e.preventDefault();
-const response=await axios.post(`http://localhost:5000/api/post/createPost`,variables)
-if(response.data.status){
-  navigate(`/community/${communityName}`)
-}else{
-  toast.error(response.data.msg,toastOptions)
-}
-}
-  
+  const [postData, setPostData] = useState({ title: '', content: '' });
+  const { communityName } = useParams();
+  const navigate = useNavigate();
+  const variables = {
+    Name: communityName,
+    writer: localStorage.getItem('userid'),
+    title: postData.title,
+    content: postData.content
+  }
+
+  const addPostHandler = async (e) => {
+    e.preventDefault();
+    const response = await axios.post(`http://localhost:5000/api/post/createPost`, variables)
+    if (response.data.status) {
+      navigate(`/community/${communityName}`)
+    } else {
+      toast.error(response.data.msg, toastOptions)
+    }
+  }
+
   return (
-    <div className='createPostContainer'>
+    <div className='createPostContainer col-12 col-md-6'>
       <div className='createPostHeader'>
-      Create a post
-      <hr/>
+        Create a post
+        <hr />
       </div>
-      <div className='createPostBody'>
-      <form onSubmit={addPostHandler}>
-      <div class="input-group mb-3">
-  <input type="text"   onChange={(e)=>setPostData({...postData,title:e.target.value})} class="addPostInput text-light" placeholder="Add Title" aria-label="Add title" aria-describedby="basic-addon2"/>
-  <span class="input-group-text text-light" id="basic-addon2">0/300</span>
-</div>
-  <div>
-  <textarea
-        className='text-areaStyling'
-          placeholder="Text"
-          onChange={(e)=>setPostData({...postData,content:e.target.value})}
-        />
-  </div>
-  <div className='postButtonContainer'>
-  <button type="submit" class="btn btn-light">Post</button>
-  </div>
-  </form>
+      <div className='createPostBody col-12 col-md-12'>
+        <form onSubmit={addPostHandler}>
+          <div>
+
+            <div>
+              <input type="text" style={{ background: '#1A1A1B', height: '45px' }} onChange={(e) => setPostData({ ...postData, title: e.target.value })} className="addPostInput text-light col-md-11 col-10" placeholder="Add Title" id='title' maxLength={100} />
+              <label htmlFor="title" className="form-label text-light ms-1 mt-2">{postData && postData.title && postData.title.length}/100</label>
+            </div>
+          </div>
+          <div>
+            <textarea
+              className='text-areaStyling'
+              placeholder="Text"
+              onChange={(e) => setPostData({ ...postData, content: e.target.value })}
+            />
+          </div>
+          <div className='postButtonContainer'>
+
+            <button type="submit" class="btn btn-light">Post</button>
+          </div>
+        </form>
       </div>
-    
-      </div>
-      
+
+    </div>
+
   )
 }
 
