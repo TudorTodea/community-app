f7de6f9716dc0fccecb9502d748e38933a88d93d
TudorTodea <tudortodea1416@gmail.com>
2022-12-06 02:13:19 +0200

    check for authentication when liking posts

--- frontend/src/Components/LikeDislike/LikeDislike.js
+++ frontend/src/Components/LikeDislike/LikeDislike.js
@@ -1,59 +1,64 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { BiUpvote } from "react-icons/bi";
 import { BiDownvote } from "react-icons/bi";
 import axios from 'axios';
 import './LikeDislike.css'
+import AuthContext from '../../store/auth-context';
 const LikeDislike = ({ post }) => {
     const [likes, setLikes] = useState(0);
     const [LikeAction, setLikeAction] = useState(null);
     const [DislikeAction, setDislikeAction] = useState(null);
+    const authCtx = useContext(AuthContext);
     const postId = post._id;
 
     const onDislike = async (e) => {
         e.preventDefault();
-        if (DislikeAction === null) {
-            const response = await axios.post(`http://localhost:5000/api/likedislike/upDislike/`, { postId, userId: localStorage.getItem('userid') })
-            if (response.status) {
-                if (LikeAction !== null) {
-                    setLikes((prev) => prev - 2)
-                    setLikeAction(null)
-                    setDislikeAction('disliked')
-                } else {
-                    setLikes((prev) => prev - 1)
-                    setDislikeAction('disliked')
+        if (authCtx.isLoggedIn) {
+            if (DislikeAction === null) {
+                const response = await axios.post(`http://localhost:5000/api/likedislike/upDislike/`, { postId, userId: localStorage.getItem('userid') })
+                if (response.status) {
+                    if (LikeAction !== null) {
+                        setLikes((prev) => prev - 2)
+                        setLikeAction(null)
+                        setDislikeAction('disliked')
+                    } else {
+                        setLikes((prev) => prev - 1)
+                        setDislikeAction('disliked')
+                    }
                 }
-            }
-        } else {
-            const response = await axios.post(`http://localhost:5000/api/likedislike/unDislike/`, { postId, userId: localStorage.getItem('userid') })
-            if (response.status) {
-                setLikes((prev) => prev + 1)
-                setDislikeAction(null)
+            } else {
+                const response = await axios.post(`http://localhost:5000/api/likedislike/unDislike/`, { postId, userId: localStorage.getItem('userid') })
+                if (response.status) {
+                    setLikes((prev) => prev + 1)
+                    setDislikeAction(null)
+                }
             }
         }
     }
     const onLike = async (e) => {
         e.preventDefault();
-        if (LikeAction === null) {
-            const response = await axios.post(`http://localhost:5000/api/likedislike/upLike/`, { postId, userId: localStorage.getItem('userid') })
-            if (response.status) {
-                if (DislikeAction !== null) {
-                    setLikes((prev) => prev + 2)
-                    setDislikeAction(null);
-                    setLikeAction('liked');
-                } else {
-                    setLikes((prev) => prev + 1)
-                    setLikeAction('liked')
+        if (authCtx.isLoggedIn) {
+            if (LikeAction === null) {
+                const response = await axios.post(`http://localhost:5000/api/likedislike/upLike/`, { postId, userId: localStorage.getItem('userid') })
+                if (response.status) {
+                    if (DislikeAction !== null) {
+                        setLikes((prev) => prev + 2)
+                        setDislikeAction(null);
+                        setLikeAction('liked');
+                    } else {
+                        setLikes((prev) => prev + 1)
+                        setLikeAction('liked')
+                    }
                 }
-            }
-        } else {
-            const response = await axios.post(`http://localhost:5000/api/likedislike/unLike/`, { postId, userId: localStorage.getItem('userid') })
-            if (response.status) {
-                setLikes((prev) => prev - 1)
-                setLikeAction(null);
+            } else {
+                const response = await axios.post(`http://localhost:5000/api/likedislike/unLike/`, { postId, userId: localStorage.getItem('userid') })
+                if (response.status) {
+                    setLikes((prev) => prev - 1)
+                    setLikeAction(null);
+                }
             }
         }
     }
-
     useEffect(() => {
         const getLikes = async () => {
             const response = await axios.post(`http://localhost:5000/api/likedislike/getLikes/`, { postId })
