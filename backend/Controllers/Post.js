7ca2d7eae4cc6f0f75e4aa05c2d5b5a4c9325f66
TudorTodea <tudortodea1416@gmail.com>
2022-12-09 06:07:38 +0200

    added images +check if community exists on create post method

--- backend/Controllers/Post.js
+++ backend/Controllers/Post.js
@@ -6,6 +6,9 @@ module.exports.createPost = async (req, res, next) => {
             return res.json({ status: false, msg: 'Please complete all fields' })
         }
         const community = await Community.findOne({ Name: req.body.Name }).select(["_id"])
+        if (!community) {
+            return res.json({ status: false, msg: 'Community not found' })
+        }
         const post = await Post.create({
             writer: req.body.writer, communityId: community._id, title: req.body.title, content: req.body.content
         });
@@ -18,7 +21,7 @@ module.exports.createPost = async (req, res, next) => {
 module.exports.getPosts = async (req, res, next) => {
     try {
         const community = await Community.findOne({ Name: req.params.communityName }).select(["_id"])
-        const posts = await Post.find({ communityId: community._id }).populate(['communityId', 'writer']).exec(function (err, posts) {
+        const posts = await Post.find({ communityId: community._id }).sort({ _id: -1 }).populate(['communityId', 'writer']).exec(function (err, posts) {
             if (err) return res.json({ err, status: false })
             return res.json({ posts, status: true })
         });
